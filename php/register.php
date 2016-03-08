7015ca62a38632b31f9aeffcce1f18b04cb13044
vs-slavchev <vs_slavchev@abv.bg>
2016-03-08 21:51:43 +0100

    Change register functionality

--- php/register.php
+++ php/register.php
@@ -7,20 +7,25 @@ if (!$db_server) die(mysql_fatal_error("Unable to connect"));
 mysql_select_db($db_database) or die(mysql_fatal_error("Unable to select database"));
 
 if (isset($_POST['email']) &&
-    isset($_POST['password']) &&
-    isset($_POST['confirm_password']))
+    isset($_POST['first_name']) &&
+    isset($_POST['last_name']) &&
+    isset($_POST['paypal']) &&
+    isset($_POST['phone_number']))
 {
 
   $email = mysql_entities_fix_string($_POST['email']);
-  $password = mysql_entities_fix_string($_POST['password']);
-  $confirm_password = mysql_entities_fix_string($_POST['confirm_password']);
+  $first_name = mysql_entities_fix_string($_POST['first_name']);
+  $last_name = mysql_entities_fix_string($_POST['last_name']);
+  $paypal = mysql_entities_fix_string($_POST['paypal']);
+  $phone_number = mysql_entities_fix_string($_POST['phone_number']);
 
+  $query = "INSERT INTO users (email, first_name, last_name, paypal, phone_number)
+  VALUES ('$email', '$first_name', '$last_name', '$paypal', '$phone_number');";
+  $result = mysql_query($query);
+  if (!$result) die (mysql_fatal_error("Denied access"));
 
-if ($password == $confirm_password)
-{
-  add_user($email, $password);
-  header("Location: ../#/");
-  #maybe tell user to log in with their new acc
+  header("Location: ../index.html");
+
 } else
 {
   echo "<h1>You didn't input all fields!</h1>";
--- php/util.php
+++ php/util.php
@@ -1,28 +1,5 @@
 <?php
 // Util functions
-function db_connect()
-{
-  $db_server = mysql_connect($db_hostname, $db_username, $db_password);
-  if (!$db_server) die(mysql_fatal_error("Unable to connect"));
-  mysql_select_db($db_database) or die(mysql_fatal_error("Unable to select database"));
-  return $db_server;
-}
-
-function tokenize_password($email, $pass)
-{
-  $salt1 = "i!y@l" . strrev($email);
-  $salt2 = strlen($email) . "oim80";
-  return sha1("$salt1$pass$salt2");
-}
-
-function add_user($email, $password)
-{
-  $token = tokenize_password($email, $password);
-  $query = "INSERT INTO users (email, password) VALUES ('$email', '$token');";
-  $result = mysql_query($query);
-  if (!$result) die (mysql_fatal_error("Denied access"));
-}
-
 function mysql_fatal_error($msg)
 {
   $sql_msg = mysql_error();
