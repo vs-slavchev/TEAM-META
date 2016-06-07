ae3954033d9f64bbcaf3f548d5ba9da771e4dc46
Hristov,Ivaylo I.H <i.hristov@student.fontys.nl>
2016-06-07 11:28:44 +0200

    АЛЕЛУЯ!

--- Scripts/money owed cleanup.py
+++ Scripts/money owed cleanup.py
@@ -6,20 +6,23 @@ conn = MySQLdb.connect(host = "athena01.fhict.local",
 c = conn.cursor()
 
 c.execute("SELECT `qr_code`, `money`, `money_owed` FROM `user` WHERE `money`>0 AND `money_owed`>0")
-results = cursor.fetchall()
+results = c.fetchall()
+q="UPDATE user SET money=%s, money_owed = %s WHERE qr_code = ('%s')"
 for row in results:
 	qr = row[0]
 	moni = row[1]
 	gibmoni = row[2]
 	if moni > gibmoni:
 		moni, gibmoni = moni-gibmoni, 0
-		c.execute("UPDATE `user` SET `money` = ('%s'), `money_owed` = ('%s') WHERE 'qr_code' = ('%s')" % (moni, gibmoni, qr))
-	else if gibmoni > moni:
+		c.execute(q % (moni, gibmoni, qr))
+		conn.commit()
+	elif gibmoni > moni:
 		moni, gibmoni = 0, gibmoni-moni
-		c.execute("UPDATE `user` SET `money` = ('%s'), `money_owed` = ('%s') WHERE 'qr_code' = ('%s')" % (moni, gibmoni, qr))
-	else if gibmoni == moni:
+		c.execute(q % (moni, gibmoni, qr))
+		conn.commit()
+	elif gibmoni == moni:
 		moni, gibmoni = 0, 0
-		c.execute("UPDATE `user` SET `money` = ('%s'), `money_owed` = ('%s') WHERE 'qr_code' = ('%s')" % (moni, gibmoni, qr))
-		
-conn.commit()        
+		c.execute(q % (moni, gibmoni, qr))
+		conn.commit()
+
 conn.close()
