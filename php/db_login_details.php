b1927e73997dd24767430c9af831ad1ee4116d26
vs-slavchev <vs_slavchev@abv.bg>
2016-06-08 09:35:04 +0200

    Delete old files

--- php/db_login_details.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db_hostname = 'localhost';
-$db_database = 'i?';
-$db_username = 'i?';
-$db_password = 'kek';
-?>
--- php/email_hasher.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'util.php';
-
-$email = $_POST['email'];
-echo tokenize_qr_code($email);
-
-?>
