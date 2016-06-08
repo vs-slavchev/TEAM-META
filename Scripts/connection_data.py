b554db6ab11b8e78dcb30eb9ac5e1f969517425c
vs-slavchev <vs_slavchev@abv.bg>
2016-06-08 15:42:38 +0200

    Fix python scripts

---
+++ Scripts/connection_data.py
@@ -0,0 +1,4 @@
+host = "athena01.fhict.local"
+user = "dbi345959"
+passwd = "2XArGTUPc9"
+db = "dbi345959"
--- Scripts/logfilereader.py
+++ Scripts/logfilereader.py
@@ -1,8 +1,7 @@
 import MySQLdb
-conn = MySQLdb.connect(host = "athena01.fhict.local",
-                       user = "dbi345959",
-                       passwd = "2XArGTUPc9",
-                       db = "dbi345959")
+from connection_data import *
+
+conn = MySQLdb.connect(host, user, passwd, db)
 c = conn.cursor()
 
 from tkinter import *
@@ -15,7 +14,27 @@ with open(root.filename, "r") as f:
     for i in range(4): f.readline()
     for line in f:
         SLog = line.split()
-        c.execute("UPDATE USER SET money = money + ('%s'), total_money = total_money + ('%s') WHERE QR_CODE = ('%s')" % (SLog[1], SLog[1], SLog[0]))
-		
-conn.commit()        
+        qr_code = SLog[0]
+        ## kakuv data type sa parite tuka? iskam ama moga li da gi subiram kato int?
+        money_to_add = SLog[1]
+        money_previously = 0
+        money_owed = 0
+        money_current = 0
+
+        c.execute("SELECT `money`, `money_owed` FROM `user` WHERE `qr_code` = ('%s')" % (qr_code))
+        results = c.fetchall()
+
+        for row in results:
+            money_previously = row[0] ## money before the deposit
+        	money_owed = row[1]
+            money_current = money_previously + money_to_add
+        	if money_current >= money_owed:
+        		money_current, money_owed = money_current - money_owed, 0
+        	else:
+        		money_current, money_owed = 0, money_owed - money_current
+
+        ## kak se pi6e na nekolko reda bez da s4upa placeholderite? a tova query pre4i li si s gornoto?
+        c.execute("UPDATE USER SET money = ('%s'), total_money = total_money + ('%s'), money_owed = ('%s') WHERE QR_CODE = ('%s')" % (money_current, money_to_add, money_owed, qr_code))
+        conn.commit()
+
 conn.close()
